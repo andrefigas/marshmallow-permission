11e60c03cd3f7023f4ca003747b7520c47ecd6b7
Andre <andre@weway.com.br>
2016-01-03 03:10:45 -0300

    individualization of the answers of multiple requests

--- app/src/main/java/com/devfigas/permission/MarshmallowActivity.java
+++ app/src/main/java/com/devfigas/permission/MarshmallowActivity.java
@@ -1,42 +1,64 @@
 package com.devfigas.permission;
 
 import android.content.Intent;
-
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.devfigas.permission.MarshmallowPermission;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+/**
+ * Created by Andre Figas on 21/12/2015.
+ */
 public abstract class MarshmallowActivity  extends AppCompatActivity {
 
     MarshmallowPermission p;
 
 
+
     public void setCallbackPermission(MarshmallowPermission p){
-       this.p = p;
+        this.p = p;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
 
             p.getPermission();
         }
         else{
 
-            p.granted();
-        }
 
+            for(String r : p.getPermissions()){
+
+                p.granted(r);
+            }
+
+        }
     }
 
     public void onRequestPermissionsResult (int requestCode, String[] permissions, int[] grantResults){
 
         p.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(p.allowed()){
-            if(requestCode ==  p.getRequestCode()){
 
-                p.granted();
+        HashMap<String, Boolean> results = p.getResults();
+
+        for(String r : results.keySet()){
+
+            if(results.get(r)){
+                if(requestCode ==  p.getRequestCode()){
+
+                    p.granted(r);
+                }
+            }
+            else{
+                if(requestCode ==  p.getRequestCode()){
+
+                    p.denied(r);
+                }
             }
         }
-        else{
-            if(requestCode ==  p.getRequestCode()){
 
-                p.denied();
-            }
-        }
+
     }
 
     @Override
@@ -45,6 +67,9 @@ public abstract class MarshmallowActivity  extends AppCompatActivity {
         p.onActivityResult(requestCode, resultCode, data);
     }
     private void configDialog(String text, String positive, String negative){
-        p.configDialog(text,positive,negative);
+        p.configDialog(text, positive, negative);
     }
+    //public boolean result(String permission){
+    //    return p.result(permission);
+    //}
 }
